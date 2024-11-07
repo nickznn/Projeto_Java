6bc0a519402d32078edbe9829b8e550524c9d45e
nickznn <oliveiranicolas179@GMAIL.COM>
2024-11-06 22:14:06 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+package construtor;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Menu menu1 = new Menu(1);
+        menu1.exibirMenuPrincipal();
+
+        Menu menu2 = new Menu(2);
+        menu2.exibirMenuPrincipal();
+    }
+}
